e90b16ae8d592d2514e3742890ea7bcec7d98443
Saurabh Tapkir <saurabhtapkir3578@gmail.com>
2025-11-15 15:15:04 +0530

    styling installed

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const App=()=>{
+    return(
+        <div>
+            This is applayout
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>)
